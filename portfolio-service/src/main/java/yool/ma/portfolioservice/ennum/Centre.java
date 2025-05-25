f36288d75176474cb68b0c32a8c0f622d86b36fa
mervet-robot <mervetrobot@gmail.com>
2025-05-25 17:32:57 +0100

    NUN FIX-frontend-- certf-att link-icon verif-portf-bio load-centre

--- portfolio-service/src/main/java/yool/ma/portfolioservice/ennum/Centre.java
+++ portfolio-service/src/main/java/yool/ma/portfolioservice/ennum/Centre.java
@@ -3,5 +3,9 @@ package yool.ma.portfolioservice.ennum;
 public enum Centre {
     LAYOUN_CENTRE,
     DAKHLA_CENTRE,
-    CASA_CENTRE
+    CASA_CENTRE,
+    FES_CENTRE,
+    TANJE_CENTRE,
+    SALE_CENTRE,
+
 } 
